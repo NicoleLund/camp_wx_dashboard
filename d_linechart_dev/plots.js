ffb490dd0d99389d88ad92abd2ba3b082c9c5a50
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-07-15 13:26:14 -0700

    Identify JSON data of interest

--- d_linechart_dev/plots.js
+++ d_linechart_dev/plots.js
@@ -1,2 +1,9 @@
 // Plotly stacked line charts of Temp, Winds, & Precip 
 // See https://plotly.com/javascript/subplots/#stacked-subplots-with-a-shared-x-axis
+
+// json data:
+// forecasted_temperature_degC = result.properties.temperature.values[i].value
+// forecasted_windSpeed_km_per_h = result.properties.windSpeed.values[i].value
+// forecasted_windGust_km_per_h = result.properties.windGust.values[i].value
+// forecasted_probabilityOfPrecipitation = result.properties.probabilityOfPrecipitation.values[i].value
+// forecasted_quantityOfPrecipitation_mm = result.properties.quantitativePrecipitation.values[i].value
