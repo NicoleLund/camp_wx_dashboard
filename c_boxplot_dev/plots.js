b660383b8cffeabfbfff5a1aba8ac2b81c2a4196
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-07-15 13:25:26 -0700

    Identify boxplot temperature data in json

--- c_boxplot_dev/plots.js
+++ c_boxplot_dev/plots.js
@@ -1,2 +1,5 @@
 // Plotly box plot of all forecasted temperatures for each campground 
 // See https://plotly.com/javascript/box-plots/
+
+// json data:
+// forecasted_temperature_degC = result.properties.temperature.values[i].value
