64947c5dac156d0c8e56339112880591e1eac994
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-07-15 14:36:07 -0700

    Add json urls to plots.js

--- c_boxplot_dev/plots.js
+++ c_boxplot_dev/plots.js
@@ -3,3 +3,8 @@
 
 // json data:
 // forecasted_temperature_degC = result.properties.temperature.values[i].value
+var whitetail_url = "https://api.weather.gov/gridpoints/TWC/100,55"
+var madera_url = "https://api.weather.gov/gridpoints/TWC/91,27"
+var showlow_url = "https://api.weather.gov/gridpoints/FGZ/127,37"
+var crescentMoon_url = "https://api.weather.gov/gridpoints/FGZ/65,72"
+var indianGarden_url = "https://api.weather.gov/gridpoints/FGZ/61,129"
