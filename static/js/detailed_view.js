f67daab0b9ca80ed2e6834002e86e8a3f3562d91
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-07-23 11:11:56 -0700

    Add fire danger text

--- static/js/detailed_view.js
+++ static/js/detailed_view.js
@@ -3,16 +3,17 @@ var location_display = d3.select("#selected_campground").text();
 switch(location_display) {
     case "Bog Springs Campground":
         d3.json("api/bog_springs.json").then((detailed_data) => {
-            console.log('Bog Springs Data')
+            console.log('Bog Springs Data');
             console.log(detailed_data);
 
+            build_page(detailed_data);
 
         });
         break;
     
     case "Rose Canyon Campground":
         d3.json("api/rose_canyon.json").then((detailed_data) => {
-            console.log('Rose Canyon Data')
+            console.log('Rose Canyon Data');
             console.log(detailed_data);
 
 
@@ -21,7 +22,7 @@ switch(location_display) {
 
     case "Spencer Canyon Campground":
         d3.json("api/spencer_canyon.json").then((detailed_data) => {
-            console.log('Spencer Canyon Data')
+            console.log('Spencer Canyon Data');
             console.log(detailed_data);
 
 
@@ -30,9 +31,16 @@ switch(location_display) {
 
     default:
         d3.json("api/bog_springs.json").then((detailed_data) => {
-            console.log('Bog Springs Data')
+            console.log('Bog Springs Data');
             console.log(detailed_data);
 
 
         });
+};
+
+function build_page(data) {
+    // Add fire danger level
+    d3.select("#fire_level")
+        .append("p")
+        .text(data[0].fire_danger);
 };
