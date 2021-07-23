7199d098b15bf65be222278be2c5b01c86ba3706
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-07-23 09:10:54 -0700

    Pass data to detailed_view

--- static/js/detailed_view.js
+++ static/js/detailed_view.js
@@ -1,20 +1,38 @@
-d3.json("api/bog_springs.json").then((detailed_data) => {
-    console.log('Bog Springs Data')
-    console.log(detailed_data);
+var location_display = d3.select("#selected_campground").text();
+
+switch(location_display) {
+    case "Bog Springs Campground":
+        d3.json("api/bog_springs.json").then((detailed_data) => {
+            console.log('Bog Springs Data')
+            console.log(detailed_data);
 
 
-});
-
-d3.json("api/rose_canyon.json").then((detailed_data) => {
-    console.log('Rose Canyon Data')
-    console.log(detailed_data);
-
-
-});
-
-d3.json("api/spencer_canyon.json").then((detailed_data) => {
-    console.log('Spencer Canyon Data')
-    console.log(detailed_data);
+        });
+        break;
+    
+    case "Rose Canyon Campground":
+        d3.json("api/rose_canyon.json").then((detailed_data) => {
+            console.log('Rose Canyon Data')
+            console.log(detailed_data);
 
 
-});
+        });
+        break;
+
+    case "Spencer Canyon Campground":
+        d3.json("api/spencer_canyon.json").then((detailed_data) => {
+            console.log('Spencer Canyon Data')
+            console.log(detailed_data);
+
+
+        });
+        break;
+
+    default:
+        d3.json("api/bog_springs.json").then((detailed_data) => {
+            console.log('Bog Springs Data')
+            console.log(detailed_data);
+
+
+        });
+};
