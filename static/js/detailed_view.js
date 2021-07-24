c2fa7108df61faf309e6a6c26631c5b17bef9af4
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-07-24 11:44:17 -0700

    Insert line_chart code to js file

--- static/js/detailed_view.js
+++ static/js/detailed_view.js
@@ -52,10 +52,11 @@ function build_page(data) {
         .html(data[0].campground + ' Forest Service Webpage');
         
     // Add line chart to carousel
-    d3.select("#line_chart")
-        .append("div")
-        .html("<img src='https://pandas.pydata.org/pandas-docs/stable/_images/frame_plot_subplots.png'>");
-        
+    // d3.select("#line_chart")
+    //     .append("div")
+    //     .html("<img src='https://pandas.pydata.org/pandas-docs/stable/_images/frame_plot_subplots.png'>");
+    line_chart(data);
+
     // Add google map to carousel
     d3.select("#google_map")
         .append("div")
@@ -66,3 +67,75 @@ function build_page(data) {
         .append("div")
         .html("<img src='" + data[0].campsite_url +"'>");
 };
+
+function line_chart(data) {
+    var temps = data[1].forecasted_temperature_degF
+    var temp_times = data[1].forecastTime_temperature
+    var windGust = data[1].forecasted_windGust_miles_per_h
+    var windGust_times = data[1].forecastTime_windGust
+    var windSpeed = data[1].forecasted_windSpeed_miles_per_h
+    var windSpeed_times = data[1].forecastTime_windSpeed
+    var precipitation = data[1].forecasted_probabilityOfPrecipitation
+    var precipitation_times = data[1].forecastTime_probabilityOfPrecipitation
+    var quantityPrecipitation = data[1].forecasted_quantityOfPrecipitation_mm
+    var quantityPrecipitation_times = data[1].forecastTime_quantityOfPrecipitation
+
+    var trace1 = {
+        x: windSpeed_times,
+        y: windSpeed,
+        // xaxis: '%d %B (%a)<br>%Y',
+        mode: 'lines+markers',
+        type: 'scatter'
+      };
+      
+      var trace2 = {
+        x: windGust_times,
+        y: windGust,
+        xaxis: 'x2',
+        yaxis: 'y2',
+        mode: 'lines+markers',
+        type: 'scatter'
+      };
+      
+      var trace3 = {
+        x: temp_times,
+        y: temps,
+        xaxis: 'x3',
+        yaxis: 'y3',
+        mode: 'lines+markers',
+        type: 'scatter'
+      };
+
+      var trace4 = {
+        x: precipitation_times,
+        y: precipitation,
+        xaxis: 'x4',
+        yaxis: 'y4',
+        mode: 'lines+markers',
+        type: 'scatter'
+      };
+      
+      var trace5 = {
+        x: quantityPrecipitation_times,
+        y: quantityPrecipitation,
+        xaxis: 'x5',
+        yaxis: 'y5',
+        mode: 'lines+markers',
+        type: 'scatter'
+        
+      };
+      
+      var data = [trace1, trace2,trace3,trace4,trace5];
+      
+      var layout = {
+      grid: {
+          rows: 5,
+          columns: 1,
+          pattern: 'independent',
+          roworder: 'bottom to top'}
+      };
+      
+      // Plot traces (probability and quantity of precipitation)
+      Plotly.newPlot('line_chart', data, layout);
+
+}
